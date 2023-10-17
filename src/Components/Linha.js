602247bcd2b39ea15ca270c6322336039e11fd52
Navo <110341514+Navomeister@users.noreply.github.com>
2023-10-17 16:10:17 -0300

    Delete src directory

--- src/Components/Linha.js
+++
@@ -1,8 +0,0 @@
-import { View } from "react-native";
-import Fundos from "../Styles/Fundos";
-
-export default function Linha(){
-    return(
-        <View style={[Fundos.fundoPrimario, {height: 3}]}/>
-    )
-}
